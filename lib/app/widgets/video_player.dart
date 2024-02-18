5df7508714ee0935ec60334dd7601a57a9920489
devlow03 <nth4356@gmail.com>
2024-02-18 07:49:10 +0000

    fix: seek to video 10s

--- lib/app/widgets/video_player.dart
+++ lib/app/widgets/video_player.dart
@@ -1,7 +1,7 @@
-import 'package:app_ft_movies/app/core/global_color.dart';
 import 'package:flutter/material.dart';
 import 'package:better_player/better_player.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart';
 
 class ChewieVideoPlayer extends StatefulWidget {
   final String videoUrl;
@@ -25,6 +25,10 @@ class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
   late SharedPreferences _prefs;
   late String _prefsKey;
 
+  double _progress = 0.0;
+  String _videoDuration = "00:00";
+  String _currentTime = "00:00";
+
   @override
   void initState() {
     super.initState();
@@ -34,13 +38,60 @@ class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: GlobalColor.backgroundColor,
-      appBar: AppBar(
-        foregroundColor: Colors.white,
-        backgroundColor: GlobalColor.backgroundColor,
+    return Shortcuts(
+      shortcuts: <LogicalKeySet, Intent>{
+                    LogicalKeySet(LogicalKeyboardKey.select):
+                        const ActivateIntent(),
+                  },
+      child: RawKeyboardListener(
+        focusNode: FocusNode(),
+        onKey: (RawKeyEvent event) {
+          if (event.logicalKey == LogicalKeyboardKey.select) {
+           _handlePlayPause();
+          }
+        },
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Stack(
+            children: [
+              BetterPlayer(controller: _betterPlayerController),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  color: Colors.black.withOpacity(0.5),
+                  child: Row(
+                    children: [
+                      Text(
+                        _currentTime,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Slider(
+                          value: _progress,
+                          onChanged: (newValue) {
+                            
+                            setState(() {
+                              _progress = newValue;
+                            });
+                            _onProgressChanged(newValue);
+                          },
+                        ),
+                      ),
+                      Text(
+                        _videoDuration,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: BetterPlayer(controller: _betterPlayerController),
     );
   }
 
@@ -59,20 +110,24 @@ class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
 
       _betterPlayerController = BetterPlayerController(
         BetterPlayerConfiguration(
-
           overlay: Padding(
             padding: const EdgeInsets.all(8),
             child: Text("${widget.fileName}-${widget.episode}"),
           ),
-          autoPlay: true,
+          autoPlay: false,
           looping: false,
-          // aspectRatio: 16 / 9,
-          fullScreenByDefault: true,
-
           controlsConfiguration: BetterPlayerControlsConfiguration(
             controlBarColor: Colors.transparent,
+            enableProgressText: false,
+            enableFullscreen: false,
+            enableOverflowMenu: false,
+            enableSkips: false,
+            enablePlaybackSpeed: false,
+            enableProgressBar: false,
+            enablePlayPause: false,
+            enableMute: false,
+          
           ),
-
         ),
         betterPlayerDataSource: BetterPlayerDataSource(
           BetterPlayerDataSourceType.network,
@@ -82,15 +137,27 @@ class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
 
       // Listen to video player events
       _betterPlayerController.addEventsListener((event) {
-         if (event.betterPlayerEventType == BetterPlayerEventType.progress) {
+        if (event.betterPlayerEventType == BetterPlayerEventType.progress) {
           _savePosition();
-          print(">>>>>>>>>>>>>>${_betterPlayerController.videoPlayerController!.value.position.inSeconds}");
+          setState(() {
+            _progress = _betterPlayerController
+                    .videoPlayerController!.value.position.inSeconds /
+                _betterPlayerController
+                    .videoPlayerController!.value.duration!.inSeconds;
+            _currentTime = _formatDuration(
+                _betterPlayerController
+                    .videoPlayerController!.value.position);
+            _videoDuration = _formatDuration(
+                _betterPlayerController
+                    .videoPlayerController!.value.duration!);
+          });
         }
-        if (event.betterPlayerEventType == BetterPlayerEventType.initialized) {
+        if (event.betterPlayerEventType ==
+            BetterPlayerEventType.initialized) {
           // Video player initialized, seek to saved position if available
           if (savedPosition != null) {
-            _betterPlayerController.seekTo(Duration(seconds: savedPosition));
-            print(">>>>>>position: $savedPosition");
+            _betterPlayerController
+                .seekTo(Duration(seconds: savedPosition));
           }
         }
       });
@@ -105,7 +172,85 @@ class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
   }
 
   void _savePosition() {
-    int positionInSeconds = _betterPlayerController.videoPlayerController!.value.position.inSeconds;
+    int positionInSeconds = _betterPlayerController
+        .videoPlayerController!.value.position.inSeconds;
     _prefs.setInt(_prefsKey, positionInSeconds);
+  }
+
+  void _onProgressChanged(double newValue) {
+  Duration? duration =
+      _betterPlayerController.videoPlayerController!.value.duration;
+  
+  if (duration != null) {
+    int newPositionInSeconds = (duration.inSeconds * newValue).round();
+    int currentPosInSeconds = _betterPlayerController.videoPlayerController!.value.position.inSeconds;
+    
+    // Xác định chiều tua
+    if (newPositionInSeconds > currentPosInSeconds) {
+      // Tua tới
+      int seekToPosition = (currentPosInSeconds + 10).clamp(0, duration.inSeconds).toInt();
+      double newProgress = seekToPosition / duration.inSeconds;
+      
+      setState(() {
+        _progress = newProgress;
+      });
+
+      _betterPlayerController.seekTo(Duration(seconds: seekToPosition));
+    } else {
+      // Tua lại
+      int seekToPosition = (currentPosInSeconds - 10).clamp(0, duration.inSeconds).toInt();
+      double newProgress = seekToPosition / duration.inSeconds;
+      
+      setState(() {
+        _progress = newProgress;
+      });
+
+      _betterPlayerController.seekTo(Duration(seconds: seekToPosition));
+    }
+  }
+}
+
+
+
+ // Biến lưu trữ trạng thái phát trước đó
+
+bool isHandlingKeyPress = false;
+
+void _handlePlayPause() {
+  if (isHandlingKeyPress) {
+    return; // Nếu hàm đang được thực thi, bỏ qua các lần gọi tiếp theo
+  }
+  
+  isHandlingKeyPress = true;
+
+  bool isPlaying = _betterPlayerController.isPlaying()!;
+  
+  if (isPlaying) {
+    _betterPlayerController.pause();
+    
+  } else {
+    _betterPlayerController.play();
+    
+  }
+
+  Future.delayed(Duration(milliseconds: 300), () {
+    isHandlingKeyPress = false;
+  });
+}
+
+
+
+
+
+
+
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes =
+        twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds =
+        twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
   }
 }
