d2d1345d1e21508ec07ad806a229c8f1f7069a06
devlow03 <nth4356@gmail.com>
2024-03-23 10:57:52 +0000

    push src

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,8 +32,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4f1ebd65292b85b0223cb72a06603b9
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/logo_app.png": "006994678b26a74756a03d5597e7ae8e",
-"index.html": "91e465af488b41f6a3e1de718d1aa7ae",
-"/": "91e465af488b41f6a3e1de718d1aa7ae",
+"index.html": "86af58731ed9c06c23daf6a12fb7ef74",
+"/": "86af58731ed9c06c23daf6a12fb7ef74",
 "main.dart.js": "c4e2b04592562a9debf2d93ced4364f7",
 "manifest.json": "aace55545452047f6f4963f0ae468a58",
 "version.json": "103ab17c991dc677c197acb44fd2ae48"};
